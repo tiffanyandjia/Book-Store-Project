5ab91cfaf257f9dabb7956fdd3eaaf97bef96611
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-01-29 21:48:58 -0500

    save a new book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,41 @@ import mongoose from "mongoose";
 
 const app = express();
 
+//Middleware for parsing request body
+app.use(express.json());
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome To YOYO Land");
 });
 
+//Route for Save a bew Book 
+app.post('/book', async (request, response) => {
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message:"Send all required fields: title, author, publishYear",
+            });
+        }
+        const newBook = {
+            title:request.body.title,
+            author:request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        
+        const book = await Book.create(newBook);
 
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+        
+    }
+});
 
 mongoose.connect(mongoDBURL).then(()=>{
     console.log("App connected to database");
