16f64878cec4656401336267e19326d08b4287f4
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-01-30 16:18:10 -0500

    CORS

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,18 @@ const app = express();
 //Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handing CORS POLICY
+//Option 1: Allow All Origins with Default of cors(*)
+//app.use(cors());
+//Option 2: Allow Custom Origins
+app.use(
+    cors({
+        origin:'http://localhost:3000',
+        methods:['GET','POST','PUT','DELETE'],
+        allowedHeaders:['Content-Type'],
+    })
+);
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome To YOYO Land");
