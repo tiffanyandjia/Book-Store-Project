9e1c914abafa18e3154f6b41e1dce3ab53a5fcd0
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-01-30 21:10:36 -0500

    react

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,6 @@ app.use(express.json());
 //Option 1: Allow All Origins with Default of cors(*)
 //app.use(cors());
 //Option 2: Allow Custom Origins
-app.use(
-    cors({
-        origin:'http://localhost:3000',
-        methods:['GET','POST','PUT','DELETE'],
-        allowedHeaders:['Content-Type'],
-    })
-);
 
 app.get('/', (request, response) => {
     console.log(request);
