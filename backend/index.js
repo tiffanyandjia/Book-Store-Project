460046358c6effcfb79a020ede3346725fc77caf
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-02-01 17:13:40 -0500

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,41 @@
-import { express } from "express";
-import {PORT, mongoDBURL} from "./config.js";
-import mongoose from "mongoose";
-import {Book} from './models/bookModel.js';
-import bookRoute from './routes/booksRoute.js';
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
-//Middleware for parsing request body
+// Middleware for parsing request body
 app.use(express.json());
 
-//Middleware for handing CORS POLICY
-//Option 1: Allow All Origins with Default of cors(*)
-//app.use(cors());
-//Option 2: Allow Custom Origins
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(234).send("Welcome To YOYO Land");
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
 app.use('/books', booksRoute);
 
-mongoose.connect(mongoDBURL).then(()=>{
-    console.log("App connected to database");
-})
-.catch((error)=> {
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
     console.log(error);
-});
+  });
--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,22 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const bookSchema = mongoose.Schema(
-    {title:{
-        type:String,
-        required:true,
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    author:{
-        type:String,
-        required:true,
+    author: {
+      type: String,
+      required: true,
     },
     publishYear: {
-        type:Number,
-        required: true,
+      type: Number,
+      required: true,
     },
-},
-{
+  },
+  {
     timestamps: true,
-}
+  }
 );
 
-export const Book = mongoose.model('Cat', bookSchema);
+export const Book = mongoose.model('Book', bookSchema);
